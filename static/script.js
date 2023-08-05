f2d8ee98f632fbb0c30deaf2cc76f39d104f5499
Hazukison <coolbennett@hotmail.com>
2023-08-05 21:10:06 +0100

    Increase text audio limit

--- static/script.js
+++ static/script.js
@@ -2,6 +2,7 @@ const ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev'
 
 const TEXT_BYTE_LIMIT = 287
 const textEncoder = new TextEncoder()
+const CHUNK_BYTE_LIMIT = 32232; // Maximum size per API request chunk
 
 window.onload = () => {
     document.getElementById('charcount').textContent = `0/${TEXT_BYTE_LIMIT}`
@@ -36,11 +37,27 @@ const clearError = () => {
     document.getElementById('errortext').innerHTML = 'There was an error.'
 }
 
+
 const setAudio = (base64, text) => {
-    document.getElementById('success').style.display = 'block'
-    document.getElementById('audio').src = `data:audio/mpeg;base64,${base64}`
-    document.getElementById('generatedtext').innerHTML = `"${text}"`
-}
+    // Check if the base64 audio is too long for data URI
+    if (base64.length > CHUNK_BYTE_LIMIT) {
+        setError("Audio is too long. Please try a shorter text.");
+        return;
+    }
+
+    document.getElementById('success').style.display = 'block';
+    // Check if the base64 audio is within URL length limit
+    if (base64.length <= 20000) {
+        // Use data URI for short audio files
+        document.getElementById('audio').src = `data:audio/mpeg;base64,${base64}`;
+    } else {
+        // Use Blob URL for long audio files
+        const blob = b64toBlob(base64, 'audio/mpeg');
+        const blobUrl = URL.createObjectURL(blob);
+        document.getElementById('audio').src = blobUrl;
+    }
+    document.getElementById('generatedtext').innerHTML = `"${text}"`;
+};
 
 const clearAudio = () => {
     document.getElementById('success').style.display = 'none'
@@ -72,6 +89,23 @@ const onTextareaInput = () => {
         document.getElementById('charcount').style.color = 'black'
     }
 }
+
+// Function to convert base64 to Blob
+const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
+    const byteCharacters = atob(b64Data);
+    const byteArrays = [];
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        const slice = byteCharacters.slice(offset, offset + sliceSize);
+        const byteNumbers = new Array(slice.length);
+        for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+        const byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+    }
+    const blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+};
 
 // Function to split the text into chunks of size TEXT_BYTE_LIMIT
 const splitTextIntoChunks = (text) => {
