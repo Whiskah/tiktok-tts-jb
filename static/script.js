d12bb9b8d2bd796b46f22f7326c04cd4ec778485
Hazukison <coolbennett@hotmail.com>
2023-08-06 10:47:22 +0100

    Full Restart of both files

--- static/script.js
+++ static/script.js
@@ -1,42 +1,42 @@
-const ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev'
+const ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev';
 
-const TEXT_BYTE_LIMIT = 287
-const CHUNK_BYTE_LIMIT = 277; // Maximum size per API request chunk
+const TEXT_BYTE_LIMIT = 287;
+const CHUNK_BYTE_LIMIT = 32232; // Maximum size per API request chunk
 
-const textEncoder = new TextEncoder()
+const textEncoder = new TextEncoder();
 
 window.onload = () => {
-    document.getElementById('charcount').textContent = `0/${TEXT_BYTE_LIMIT}`
-    const req = new XMLHttpRequest()
-    req.open('GET', `${ENDPOINT}/api/status`, false)
-    req.send()
+    document.getElementById('charcount').textContent = `0/${TEXT_BYTE_LIMIT}`;
+    const req = new XMLHttpRequest();
+    req.open('GET', `${ENDPOINT}/api/status`, false);
+    req.send();
 
-    let resp = JSON.parse(req.responseText)
+    let resp = JSON.parse(req.responseText);
     if (resp.data) {
         if (resp.data.available) {
-            console.info(`${resp.data.meta.dc} (age ${resp.data.meta.age} minutes) is able to provide service`)
-            enableControls()
+            console.info(`${resp.data.meta.dc} (age ${resp.data.meta.age} minutes) is able to provide service`);
+            enableControls();
         } else {
-            console.error(`${resp.data.meta.dc} (age ${resp.data.meta.age} minutes) is unable to provide service`)
+            console.error(`${resp.data.meta.dc} (age ${resp.data.meta.age} minutes) is unable to provide service`);
             setError(
                 `Service not available${resp.data.message && resp.data.message.length > 1 ? ` (<b>"${resp.data.message}"</b>)` : ''}, try again later or check the <a href='https://github.com/Weilbyte/tiktok-tts'>GitHub</a> repository for more info`
-            )
+            );
         }
     } else {
-        setError('Error querying API status, try again later or check the <a href=\'https://github.com/Weilbyte/tiktok-tts\'>GitHub</a> repository for more info')
+        setError('Error querying API status, try again later or check the <a href=\'https://github.com/Weilbyte/tiktok-tts\'>GitHub</a> repository for more info');
     }  
-}
+};
 
 const setError = (message) => {
-    clearAudio()
-    document.getElementById('error').style.display = 'block'
-    document.getElementById('errortext').innerHTML = message
-}
+    clearAudio();
+    document.getElementById('error').style.display = 'block';
+    document.getElementById('errortext').innerHTML = message;
+};
 
 const clearError = () => {
-    document.getElementById('error').style.display = 'none'
-    document.getElementById('errortext').innerHTML = 'There was an error.'
-}
+    document.getElementById('error').style.display = 'none';
+    document.getElementById('errortext').innerHTML = 'There was an error.';
+};
 
 const setAudio = (base64, text) => {
     // Check if the base64 audio is too long for data URI
@@ -60,35 +60,35 @@ const setAudio = (base64, text) => {
 };
 
 const clearAudio = () => {
-    document.getElementById('success').style.display = 'none'
-    document.getElementById('audio').src = ``
-    document.getElementById('generatedtext').innerHTML = ''
-}
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('audio').src = ``;
+    document.getElementById('generatedtext').innerHTML = '';
+};
 
 const disableControls = () => {
-    document.getElementById('text').setAttribute('disabled', '')
-    document.getElementById('voice').setAttribute('disabled', '')
-    document.getElementById('submit').setAttribute('disabled', '')
-}
+    document.getElementById('text').setAttribute('disabled', '');
+    document.getElementById('voice').setAttribute('disabled', '');
+    document.getElementById('submit').setAttribute('disabled', '');
+};
 
 const enableControls = () => {
-    document.getElementById('text').removeAttribute('disabled')
-    document.getElementById('voice').removeAttribute('disabled')
-    document.getElementById('submit').removeAttribute('disabled')
-}
+    document.getElementById('text').removeAttribute('disabled');
+    document.getElementById('voice').removeAttribute('disabled');
+    document.getElementById('submit').removeAttribute('disabled');
+};
 
 const onTextareaInput = () => {
-    const text = document.getElementById('text').value
-    const textEncoded = textEncoder.encode(text)
+    const text = document.getElementById('text').value;
+    const textEncoded = textEncoder.encode(text);
 
-    document.getElementById('charcount').textContent = `${textEncoded.length <= 999 ? textEncoded.length : 999}/${TEXT_BYTE_LIMIT}`
+    document.getElementById('charcount').textContent = `${textEncoded.length <= 999 ? textEncoded.length : 999}/${TEXT_BYTE_LIMIT}`;
 
     if (textEncoded.length > TEXT_BYTE_LIMIT) {
-        document.getElementById('charcount').style.color = 'red'
+        document.getElementById('charcount').style.color = 'red';
     } else {
-        document.getElementById('charcount').style.color = 'black'
+        document.getElementById('charcount').style.color = 'black';
     }
-}
+};
 
 // Function to convert base64 to Blob
 const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
@@ -108,10 +108,10 @@ const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
 };
 
 const submitForm = () => {
-    clearError()
-    clearAudio()
+    clearError();
+    clearAudio();
 
-    disableControls()
+    disableControls();
 
     let text = document.getElementById('text').value;
     // Remove excessive line breaks from the text
@@ -130,7 +130,7 @@ const submitForm = () => {
         // Check if adding the current word will exceed the byte limit
         const nextChunk = `${currentChunk}${currentChunk ? ' ' : ''}${word}`;
         const nextChunkLength = new TextEncoder().encode(nextChunk).length;
-        
+
         if (nextChunkLength <= TEXT_BYTE_LIMIT) {
             // Add the word to the current chunk if it doesn't exceed the limit
             currentChunk = nextChunk;
@@ -146,16 +146,16 @@ const submitForm = () => {
         textChunks.push(currentChunk);
     }
 
-    const textLength = new TextEncoder().encode(text).length
-    console.log(textLength)
+    const textLength = new TextEncoder().encode(text).length;
+    console.log(textLength);
 
-    if (textLength === 0) text = 'The fungus among us.' 
-    const voice = document.getElementById('voice').value
+    if (textLength === 0) text = 'The fungus among us.'; 
+    const voice = document.getElementById('voice').value;
 
-    if(voice == "none") {
+    if (voice == "none") {
         setError("No voice has been selected");
-        enableControls()
-        return
+        enableControls();
+        return;
     }
 
     if (textLength > TEXT_BYTE_LIMIT) {
@@ -163,7 +163,7 @@ const submitForm = () => {
     } else {
         generateAudio(text, voice);
     }
-}
+};
 
 const processLongText = (text, voice) => {
     const chunks = [];
