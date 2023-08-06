b9cc993009fb1ddebc443aa5b36a5b7f6b07d133
Hazukison <coolbennett@hotmail.com>
2023-08-06 18:53:14 +0100

    FRESH START FROM MY OWN PASTES
    FRESH START FROM MY OWN PASTES

--- static/script.js
+++ static/script.js
@@ -1,62 +1,56 @@
-const ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev';
+const ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev'
 
-const TEXT_CHAR_LIMIT = 287;
-const CHUNK_CHAR_LIMIT = 99999;
-
-const textEncoder = new TextEncoder();
+const TEXT_BYTE_LIMIT = 287
+const textEncoder = new TextEncoder()
+const CHUNK_BYTE_LIMIT = 32232; // Maximum size per API request chunk
 
 window.onload = () => {
-    document.getElementById('charcount').textContent = `0/${TEXT_CHAR_LIMIT}`;
+    document.getElementById('charcount').textContent = `0/${TEXT_BYTE_LIMIT}`
+    const req = new XMLHttpRequest()
+    req.open('GET', `${ENDPOINT}/api/status`, false)
+    req.send()
 
-    const req = new XMLHttpRequest();
-    req.open('GET', `${ENDPOINT}/api/status`, false);
-    req.send();
-
-    let resp = JSON.parse(req.responseText);
+    let resp = JSON.parse(req.responseText)
     if (resp.data) {
         if (resp.data.available) {
-            console.info(`${resp.data.meta.dc} (age ${resp.data.meta.age} minutes) is able to provide service`);
-            enableControls();
+            console.info(`${resp.data.meta.dc} (age ${resp.data.meta.age} minutes) is able to provide service`)
+            enableControls()
         } else {
-            console.error(`${resp.data.meta.dc} (age ${resp.data.meta.age} minutes) is unable to provide service`);
+            console.error(`${resp.data.meta.dc} (age ${resp.data.meta.age} minutes) is unable to provide service`)
             setError(
                 `Service not available${resp.data.message && resp.data.message.length > 1 ? ` (<b>"${resp.data.message}"</b>)` : ''}, try again later or check the <a href='https://github.com/Weilbyte/tiktok-tts'>GitHub</a> repository for more info`
-            );
+                )
         }
     } else {
-        setError('Error querying API status, try again later or check the <a href=\'https://github.com/Weilbyte/tiktok-tts\'>GitHub</a> repository for more info');
+        setError('Error querying API status, try again later or check the <a href=\'https://github.com/Weilbyte/tiktok-tts\'>GitHub</a> repository for more info')
     }  
-};
+}
 
 const setError = (message) => {
-    clearAudio();
-    document.getElementById('error').style.display = 'block';
-    document.getElementById('errortext').innerHTML = message;
-};
+    clearAudio()
+    document.getElementById('error').style.display = 'block'
+    document.getElementById('errortext').innerHTML = message
+}
 
 const clearError = () => {
-    document.getElementById('error').style.display = 'none';
-    document.getElementById('errortext').innerHTML = 'There was an error.';
-};
+    document.getElementById('error').style.display = 'none'
+    document.getElementById('errortext').innerHTML = 'There was an error.'
+}
+
 
 const setAudio = (base64, text) => {
     // Check if the base64 audio is too long for data URI
-	console.log("setAudio() base64 length:" + base64.length);
-    /*if (base64.length > CHUNK_CHAR_LIMIT) {
-				console.log("setAudio() if length");
+    if (base64.length > CHUNK_BYTE_LIMIT) {
         setError("Audio is too long. Please try a shorter text.");
         return;
-    }*/
-		console.log("setAudio() pre if 0");
+    }
+
     document.getElementById('success').style.display = 'block';
-		console.log("setAudio() pre if 1");
     // Check if the base64 audio is within URL length limit
     if (base64.length <= 20000) {
-		console.log("setAudio() if");
         // Use data URI for short audio files
         document.getElementById('audio').src = `data:audio/mpeg;base64,${base64}`;
     } else {
-		console.log("setAudio() else");
         // Use Blob URL for long audio files
         const blob = b64toBlob(base64, 'audio/mpeg');
         const blobUrl = URL.createObjectURL(blob);
@@ -66,35 +60,35 @@ const setAudio = (base64, text) => {
 };
 
 const clearAudio = () => {
-    document.getElementById('success').style.display = 'none';
-    document.getElementById('audio').src = ``;
-    document.getElementById('generatedtext').innerHTML = '';
-};
+    document.getElementById('success').style.display = 'none'
+    document.getElementById('audio').src = ``
+    document.getElementById('generatedtext').innerHTML = ''
+}
 
 const disableControls = () => {
-    document.getElementById('text').setAttribute('disabled', '');
-    document.getElementById('voice').setAttribute('disabled', '');
-    document.getElementById('submit').setAttribute('disabled', '');
-};
+    document.getElementById('text').setAttribute('disabled', '')
+    document.getElementById('voice').setAttribute('disabled', '')
+    document.getElementById('submit').setAttribute('disabled', '')
+}
 
 const enableControls = () => {
-    document.getElementById('text').removeAttribute('disabled');
-    document.getElementById('voice').removeAttribute('disabled');
-    document.getElementById('submit').removeAttribute('disabled');
-};
+    document.getElementById('text').removeAttribute('disabled')
+    document.getElementById('voice').removeAttribute('disabled')
+    document.getElementById('submit').removeAttribute('disabled')
+}
 
 const onTextareaInput = () => {
-    const text = document.getElementById('text').value;
-    const textLength = textEncoder.encode(text).length;
+    const text = document.getElementById('text').value
+    const textEncoded = textEncoder.encode(text)
 
-    document.getElementById('charcount').textContent = `${textLength <= 999 ? textLength : 999}/${TEXT_CHAR_LIMIT}`;
+    document.getElementById('charcount').textContent = `${textEncoded.length <= 999 ? textEncoded.length : 999}/${TEXT_BYTE_LIMIT}`
 
-    if (textLength > TEXT_CHAR_LIMIT) {
-        document.getElementById('charcount').style.color = 'red';
+    if (textEncoded.length > TEXT_BYTE_LIMIT) {
+        document.getElementById('charcount').style.color = 'red'
     } else {
-        document.getElementById('charcount').style.color = 'black';
+        document.getElementById('charcount').style.color = 'black'
     }
-};
+}
 
 // Function to convert base64 to Blob
 const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
@@ -113,118 +107,141 @@ const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
     return blob;
 };
 
-const submitForm = async () => {
-    clearError();
-    clearAudio();
-
-    disableControls();
-
-    let text = document.getElementById('text').value;
-    // Remove excessive line breaks from the text
-    text = text.replace(/[\r\n]{3,}/g, '\n\n');
-    // Replace smart quotes with regular quotes
-    text = text.replace(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"');
-
-    // Split the text into words
+// Function to split the text into chunks of size TEXT_BYTE_LIMIT
+const splitTextIntoChunks = (text) => {
+    const chunks = [];
+    let chunk = '';
     const words = text.split(' ');
 
-    // Create an array to store the chunks of text
-    const textChunks = [];
-    let currentChunk = '';
-
     for (const word of words) {
-        // Check if adding the current word will exceed the byte limit
-        const nextChunk = `${currentChunk}${currentChunk ? ' ' : ''}${word}`;
-        const nextChunkLength = new TextEncoder().encode(nextChunk).length;
-
-        if (nextChunkLength <= TEXT_CHAR_LIMIT) {
-            // Add the word to the current chunk if it doesn't exceed the limit
-            currentChunk = nextChunk;
+        if (chunk.length + word.length + 1 <= TEXT_BYTE_LIMIT) {
+            chunk += word + ' ';
         } else {
-            // Add the current chunk to the list and start a new chunk with the current word
-            textChunks.push(currentChunk);
-            currentChunk = word;
+            chunks.push(chunk.trim());
+            chunk = word + ' ';
         }
     }
 
-    // Add the last chunk to the list
-    if (currentChunk) {
-        textChunks.push(currentChunk);
+    if (chunk.length > 0) {
+        chunks.push(chunk.trim());
     }
 
-    const textLength = new TextEncoder().encode(text).length;
-    console.log(textLength);
+    return chunks;
+};
 
-    if (textLength === 0) text = 'The fungus among us.'; 
-    const voice = document.getElementById('voice').value;
+const showLoadingOverlay = () => {
+    document.querySelector('.loading-overlay').classList.remove('hidden');
+}
+
+const hideLoadingOverlay = () => {
+    document.querySelector('.loading-overlay').classList.add('hidden');
+}
+
+const submitForm = async () => {
+    clearError()
+    clearAudio()
+
+    disableControls()
+
+    let text = document.getElementById('text').value
+    const textLength = new TextEncoder().encode(text).length
+
+    if (textLength === 0) text = 'The fungus among us.' 
+    const voice = document.getElementById('voice').value
 
     if (voice == "none") {
         setError("No voice has been selected");
-        enableControls();
-        return;
+        enableControls()
+        return
     }
 
-	if (textLength > TEXT_CHAR_LIMIT) {
-        await processLongText(text, voice); // Use await here for async handling
-    } else {
-		generateAudio(text, voice);
-    }
-};
-
-const processLongText = async (text, voice) => {
-    const chunks = [];
-    let currentIndex = 0;
-
-    while (currentIndex < text.length) {
-      const chunk = text.slice(currentIndex, currentIndex + CHUNK_CHAR_LIMIT);
-      chunks.push(chunk);
-      currentIndex += CHUNK_CHAR_LIMIT;
+    if (textLength > TEXT_BYTE_LIMIT) {
+        setError(`Text must not be over ${TEXT_BYTE_LIMIT} UTF-8 characters (currently at ${textLength})`)
+        enableControls()
+        return
     }
 
-    const audioData = [];
+    try {
+        // Show the loading popup while waiting for the audio response
+        showLoadingPopup();
 
-    for (const chunk of chunks) {
-      try {
-        const base64Audio = await generateAudio(chunk, voice);
-        audioData.push(base64Audio);
-      } catch (error) {
-        setError(
-          `<b>Generation failed</b><br/> ("${error.message || error}")`
-        );
-        return;
-      }
+        const textChunks = splitTextIntoChunks(text);
+
+        // Initialize an array to store the audio responses
+        const audioResponses = [];
+
+        for (const chunk of textChunks) {
+            const req = new XMLHttpRequest();
+            req.open('POST', `${ENDPOINT}/api/generation`, false);
+            req.setRequestHeader('Content-Type', 'application/json');
+
+            const chunkResponse = await new Promise((resolve, reject) => {
+                req.onreadystatechange = () => {
+                    if (req.readyState === XMLHttpRequest.DONE) {
+                        if (req.status === 200) {
+                            resolve(req.responseText);
+                        } else {
+                            reject(new Error('Audio request failed'));
+                        }
+                    }
+                };
+
+                req.send(JSON.stringify({
+                    text: chunk,
+                    voice: voice
+                }));
+            });
+
+            audioResponses.push(chunkResponse);
+        }
+
+        // Join the audio responses and set the audio
+        const fullAudio = audioResponses.join('');
+        setAudio(fullAudio, text);
+    } catch (error) {
+        setError('Error submitting form (printed to F12 console)');
+        console.log('^ Please take a screenshot of this and create an issue on the GitHub repository if one does not already exist :)');
+        console.log('If the error code is 503, the service is currently unavailable. Please try again later.');
+        console.log(`Voice: ${voice}`);
+        console.log(`Text: ${text}`);
     }
 
-    const mergedAudio = audioData.join('');
-    setAudio(mergedAudio, text);
+	try {
+        showLoadingOverlay(); // Show the loading overlay before making the request
+
+        const req = new XMLHttpRequest()
+        req.open('POST', `${ENDPOINT}/api/generation`, false)
+        req.setRequestHeader('Content-Type', 'application/json')
+        req.send(JSON.stringify({
+            text: text,
+            voice: voice
+        }))
+
+        let resp = JSON.parse(req.responseText)
+        if (resp.data === null) {
+            setError(`<b>Generation failed</b><br/> ("${resp.error}")`)
+        } else {
+            setAudio(resp.data, text)
+        }  
+    } catch {
+        setError('Error submitting form (printed to F12 console)')
+        console.log('^ Please take a screenshot of this and create an issue on the GitHub repository if one does not already exist :)')
+        console.log('If the error code is 503, the service is currently unavailable. Please try again later.')
+        console.log(`Voice: ${voice}`)
+        console.log(`Text: ${text}`)
+    } finally {
+        hideLoadingOverlay(); // Hide the loading overlay after the request is complete (success or error)
+    }
+
     enableControls();
 };
 
-const generateAudio = (text, voice) => {
-    return new Promise((resolve, reject) => {
-      const req = new XMLHttpRequest();
-      req.open('POST', `${ENDPOINT}/api/generation`, true); // Set to async: true
-      req.setRequestHeader('Content-Type', 'application/json');
-      req.onload = () => {
-        try {
-          const resp = JSON.parse(req.responseText);
-          if (resp.data === null) {
-            reject(resp.error || 'Unknown error');
-          } else {
-            resolve(resp.data);
-          }
-        } catch (error) {
-          reject(error);
-        }
-      };
-      req.onerror = () => {
-        reject(new Error('Error submitting form (printed to F12 console)'));
-      };
-      req.send(
-        JSON.stringify({
-          text: text,
-          voice: voice,
-        })
-      );
-    });
+// Function to show the loading popup
+const showLoadingPopup = () => {
+    document.getElementById('loading').style.display = 'flex';
+};
+
+// Function to hide the loading popup
+const hideLoadingPopup = () => {
+    document.getElementById('loading').style.display = 'none';
 };
