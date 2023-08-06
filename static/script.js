9d93cc4c6c90d88c6e4496d2a0c78d74f5243fa0
Hazukison <coolbennett@hotmail.com>
2023-08-06 19:12:10 +0100

    1

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
-const ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev'
+const ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev';
+const TEXT_BYTE_LIMIT = 287; // Update the character limit to 287
 
-const TEXT_BYTE_LIMIT = 300
 const textEncoder = new TextEncoder()
 
 window.onload = () => {
@@ -73,53 +73,109 @@ const onTextareaInput = () => {
     }
 }
 
-const submitForm = () => {
-    clearError()
-    clearAudio()
+const submitForm = async () => {
+  clearError();
+  clearAudio();
 
-    disableControls()
+  disableControls();
 
-    let text = document.getElementById('text').value
-    const textLength = new TextEncoder().encode(text).length
-    console.log(textLength)
+  let text = document.getElementById('text').value;
+  const textLength = new TextEncoder().encode(text).length;
 
-    if (textLength === 0) text = 'The fungus among us.' 
-    const voice = document.getElementById('voice').value
+  if (textLength === 0) text = 'The fungus among us.';
+  const voice = document.getElementById('voice').value;
 
-    if(voice == "none") {
-        setError("No voice has been selected");
-        enableControls()
-        return
-    }
+  if (voice == 'none') {
+    setError('No voice has been selected');
+    enableControls();
+    return;
+  }
 
-    if (textLength > TEXT_BYTE_LIMIT) {
-        setError(`Text must not be over ${TEXT_BYTE_LIMIT} UTF-8 characters (currently at ${textLength})`)
-        enableControls()
-        return
-    }
-
+  if (textLength > TEXT_BYTE_LIMIT) {
     try {
-        const req = new XMLHttpRequest()
-        req.open('POST', `${ENDPOINT}/api/generation`, false)
-        req.setRequestHeader('Content-Type', 'application/json')
-        req.send(JSON.stringify({
-            text: text,
-            voice: voice
-        }))
-
-        let resp = JSON.parse(req.responseText)
-        if (resp.data === null) {
-            setError(`<b>Generation failed</b><br/> ("${resp.error}")`)
-        } else {
-            setAudio(resp.data, text)
-        }  
-    } catch {
-        setError('Error submitting form (printed to F12 console)')
-        console.log('^ Please take a screenshot of this and create an issue on the GitHub repository if one does not already exist :)')
-        console.log('If the error code is 503, the service is currently unavailable. Please try again later.')
-        console.log(`Voice: ${voice}`)
-        console.log(`Text: ${text}`)
+      const audioResponses = await generateAudioChunks(text, voice);
+      const mergedAudio = mergeAudioChunks(audioResponses);
+      setAudio(mergedAudio, text);
+    } catch (error) {
+      setError('Error generating audio. Please try again later.');
+      console.log('Error:', error);
     }
+  } else {
+    try {
+      const req = new XMLHttpRequest();
+      req.open('POST', `${ENDPOINT}/api/generation`, false);
+      req.setRequestHeader('Content-Type', 'application/json');
+      req.send(
+        JSON.stringify({
+          text: text,
+          voice: voice,
+        })
+      );
 
-    enableControls()
-}
+      let resp = JSON.parse(req.responseText);
+      if (resp.data === null) {
+        setError(`<b>Generation failed</b><br/> ("${resp.error}")`);
+      } else {
+        setAudio(resp.data, text);
+      }
+    } catch {
+      setError('Error submitting form. Please try again later.');
+    }
+  }
+
+  enableControls();
+};
+
+const generateAudioChunks = async (text, voice) => {
+  const chunks = splitTextIntoChunks(text, TEXT_BYTE_LIMIT);
+  const audioResponses = [];
+
+  for (const chunk of chunks) {
+    try {
+      const req = new XMLHttpRequest();
+      req.open('POST', `${ENDPOINT}/api/generation`, false);
+      req.setRequestHeader('Content-Type', 'application/json');
+      req.send(
+        JSON.stringify({
+          text: chunk,
+          voice: voice,
+        })
+      );
+
+      let resp = JSON.parse(req.responseText);
+      if (resp.data === null) {
+        throw new Error(`Generation failed for chunk: "${chunk}"`);
+      } else {
+        audioResponses.push(resp.data);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  return audioResponses;
+};
+
+const splitTextIntoChunks = (text, chunkSize) => {
+  const chunks = [];
+  for (let i = 0; i < text.length; i += chunkSize) {
+    chunks.push(text.slice(i, i + chunkSize));
+  }
+  return chunks;
+};
+
+const mergeAudioChunks = (audioResponses) => {
+  // Assuming the audioResponses array contains base64-encoded audio chunks
+  // You'll need to join the audio chunks together to create the complete audio file.
+  // The specifics of joining will depend on the format of the audio data.
+  // For example, if they are MP3 files, you can concatenate the base64 strings,
+  // decode them, and then create a new base64 string for the complete audio.
+  // Ensure that the order of the audio chunks is maintained to get the correct result.
+  // Please note that I'm assuming the API responses provide base64 audio chunks.
+
+  // Example implementation (if audioResponses contains raw base64 audio data):
+  const mergedAudioData = audioResponses.join('');
+  const mergedAudio = `data:audio/mpeg;base64,${mergedAudioData}`;
+
+  return mergedAudio;
+};
